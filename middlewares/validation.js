15be9fe288602ec3199d4200df98746cb2367049
Bruninho Volotão <bruninhovolotao@gmail.com>
2024-10-02 11:25:17 -0300

    Notes
    Estruturação da criação e alteração do sistema de mensagens.

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -45,3 +45,23 @@ export function validateUserLogin (request, response, next){
 
     next()
 }
+
+export function validateCreateMassage (request, response, next){
+    const {title, description} = request.body
+
+    // Verifica se os dados estão em branco
+    if(!title){
+        return response.status(400).json({
+            message: 'Insira um e-mail válido'
+        })
+    }
+
+    // Verifica se os dados estão em branco
+    if(!description){
+        return response.status(400).json({
+            message: 'Insira uma senha válida'
+        })
+    }
+
+    next()
+}
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,93 @@
 import express from 'express'
-
 import { users } from './users'
+import { v4 as uuidv4 } from 'uuid'
+import { validateCreateMassage } from './../middlewares/validation'
 
+
+// Criar roteador de rotas
 const router = express.Router()
 
+// deixar lista de usuarios disponíveis para outras rotas
 const notes = []
+
+// Rota de criação de mensagem
+router.post('/massage', validateCreateMassage, (request, response) => {
+    const {email, title, description} = request.body
+
+    // Filtra os e-mails para verificação
+    const checkEmailCreate  = users.find(user => user.email === email)
+
+    // Verifica se o e-mail já está cadastrado
+    if(checkEmailCreate){
+        
+        //  Cria nova mensagem
+        const newMassage = {
+            id: uuidv4(),
+            title,
+            description
+        }
+
+        // Adiciona a nova mensagem ao array de mensagens
+        notes.push(newMassage)
+
+        // Retorna a resposta com a mensagem criada
+        return response.status(201).json({
+            message: 'Mensagem criada com sucesso',
+            newMassage
+        })
+    }  else {
+        return response.status(404).json({
+            message: 'Email não encontrado, verifique ou crie uma conta'
+        })
+    }
+
+
+})
+
+// Rota para leitura de mensagem
+router.get('/massage/email', (request, response) => {
+    const {email} = request.query
+
+    // Filtra os e-mails para verificação
+    const checkEmailReading = notes.find(user => user.email === email)
+
+    // Verifica se o e-mail já está cadastrado
+    if(checkEmailReading){
+        return response.status(200).json({
+            message: `Seja bem-vindo!`,
+            notes
+        })
+    } else {
+        return response.status(404).json({
+            message: 'Email não encontrado, verifique ou crie uma conta'
+        })
+    }
+    
+})
+
+// Rota para atualização de mensagem
+router.put('/massage/:id', (request, response) => {
+    const {id, title, description} = request.body
+
+    // Filtra os ids para verificação
+    const checkIdUpdate = notes.find(v => v.id === id); 
+    
+    // Retorna a mensagem se o id não for encontrado
+    if(!checkIdUpdate){    
+        return response.status(404).json({
+            message: 'Por favor, informe um id válido da mensagem'
+        })
+    }
+
+    checkIdUpdate.title = title;
+    checkIdUpdate.description = description;
+    
+    return response.status(200).json({
+        message: 'Mensagem atualizada com sucesso',
+        notes
+    })
+
+})
+
 
 export default router
